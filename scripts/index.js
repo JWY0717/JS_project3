6973140c6433dea56e758d8c61f6459af57182b1
JWY0717 <dndud6787@naver.com>
2023-01-25 12:27:43 +0900

    폭탄게임 수정 완성본

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,102 @@ let cnt = 0 ;
 //폭탄이 섞였는지 체크하는 flag변수
 let shuffleFlag = false;
 
+//메세지 표시 함수
+const showMsg = (m) => {
+    document.querySelector("#msg").innerHTML = m ;
+}
+
+//보드 내용 지우기
+const boardInit = () => {
+    const board = document.querySelectorAll(".boardBox")
+    for(let item of board) {
+        item.innerHTML = '' ;
+    }
+    cnt = 0;                //초기화 시키기!!!
+    selNum.length = 0 ;
+    console.log(board);
+}
+//보드 전체 하트 만들기
+const showAll = () => {
+    /*let idx = num.indexOf(1) + 1;
+
+    document.querySelector("#box" + idx).innerHTML =
+    `<img src="./images/hart.png" />`;
+
+
+    console.log(idx);*/
+
+    const board = document.querySelectorAll(".boardBox")
+    for(let item of board) {
+        item.innerHTML = `<img src="./images/hart.png" />` ;
+    }
+
+
+
+}
+
+
+
+//보드 숫자 누른 후 보여주기 함수
+const show = (n)  => {
+   if (!shuffleFlag) {
+        showMsg('게임 종료! 게임을 다시  시작 하세요!');
+        //boxShuffle();
+        return ;
+   }
+   console.log(n);
+   
+   //같은 곳을 여러번 눌렸을 때!!!
+   if (selNum.indexOf(n) != -1) return ;
+
+   cnt++ ; //누른 횟수
+   selNum.push(n);  // 눌러진 정보 저장
+   console.log(selNum);
+ 
+   
+   let img ;
+   if (num[n-1] == 0) 
+   
+   img = 'hart' ;
+
+   
+    else { 
+        img = 'boom' ;
+        shuffleFlag = false;
+        showMsg('게임 실패!')
+    }
+   
+
+   document.querySelector("#box" + n).innerHTML =
+             `<img src="./images/${img}.png" />`
+
+ }
+
+    if ( cnt ==8) {
+        showAll();
+        shuffleFlag = falise;
+        showMsg('게임 성공!!')
+       
+    } 
+
+//폭탄섞기 함수
+const boxShuffle = () => {
+    if(shuffleFlag){
+        showMsg('폭탄위치를 결정했습니다.게임을 진행해 주세요');
+        return;
+
+    }
+    num.sort(()=> Math.random()- 0.5);
+    shuffleFlag = true;
+    boardInit();
+    console.log(num);
+    
+ //   num.sort(()=> Math.random()- );
+ //   selNum=[];
+
+
+}
+
 
 /* DOM이 로드된 후에 클릭이벤트 연결*/
 document.addEventListener("DOMContentLoaded", ()=>{
